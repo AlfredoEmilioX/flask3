bd342e1bafbe471c4d107cdf44395ef9062fcfe6
AlfredoEmilioX <i2212749@continental.edu.pe>
2026-03-13 16:03:30 -0500

    configuracion render y supabase

--- db.py
+++ db.py
@@ -3,11 +3,15 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 def get_connection():
-    DATABASE_URL = os.getenv("postgresql://postgres:[YOUR-PASSWORD]@db.gjqjbjppnwgkulunivmt.supabase.co:5432/postgres")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if not DATABASE_URL:
+        raise Exception("DATABASE_URL no está configurada")
 
     conn = psycopg2.connect(
         DATABASE_URL,
-        cursor_factory=RealDictCursor
+        cursor_factory=RealDictCursor,
+        sslmode="require"
     )
 
     return conn
