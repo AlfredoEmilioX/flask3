4132eb747d31b9537c3a1759093d29d150e4eeb7
AlfredoEmilioX <i2212749@continental.edu.pe>
2026-04-10 12:59:16 -0500

    Corrige db.py para Render y Supabase

--- db.py
+++ db.py
@@ -1,20 +1,18 @@
 import os
 import psycopg2
-import pymysql
-
 
 def get_connection():
     database_url = os.getenv("DATABASE_URL")
 
-    # 👉 SI estás en Render / Supabase
+    # Si está en Render / Supabase
     if database_url:
         return psycopg2.connect(database_url)
 
-    # 👉 SI estás en local (XAMPP)
+    # Si estás en local con MySQL/XAMPP
+    import pymysql
     return pymysql.connect(
         host="localhost",
         user="root",
         password="",
-        database="flask_db",
-        cursorclass=pymysql.cursors.DictCursor
+        database="dietas"
     )
